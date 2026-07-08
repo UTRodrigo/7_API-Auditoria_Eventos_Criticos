6fd2730cc46f5ef70efa81ad02c0a2e34e6b05d6
Rodrigo Zamacona <rozamco31@gmail.com>
2026-07-08 13:16:28 -0600

    Preparacion para integrar con Vercel

--- index.js
+++ index.js
@@ -14,17 +14,14 @@ const port = process.env.PORT || 5100;
 const userRoutes = require("./src/routes/usuarioRoutes");
 const auditoriaRoutes = require("./src/routes/auditoriaRoutes");
 
-
 //index.js
 app.use(express.json());
 connectDB();
-
 app.use(auditoriaMiddleware);
+app.use("/users", userRoutes);
+app.use("/auditoria", auditoriaRoutes);
 
 app.listen(port, () =>
 {
     console.log(`Hello world 2. I'm listening in port ${port}`);
 });
-
-app.use("/users", userRoutes);
-app.use("/auditoria", auditoriaRoutes);
